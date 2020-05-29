9c08c5e3642ac67fb69e324d95ee463fd031b262
heuzin <matheusqs.arq@gmail.com>
2020-05-29 12:03:21 -0400

    getLocation chaining getCountry

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
+getCountry('BR').then((country) => {
+    console.log(country.name)
+}).catch((error) => {
+    console.log(error)
+})
+
 getLocation().then((data) => {
     console.log(`You are currently in ${data.city}, ${data.region} - ${data.country}!`)
+}).catch((error) => {
+    console.log(error)
+})
+
+getLocation().then((location) => {
+    return getCountry(location.country)
+}).then((country) => {
+    console.log(country.name)
 }).catch((error) => {
     console.log(error)
 })
