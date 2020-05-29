d8454c4dd31ad1e465580833051641d322d43313
heuzin <matheusqs.arq@gmail.com>
2020-05-29 14:24:13 -0400

    async function

--- requests.js
+++ requests.js
@@ -1,24 +1,23 @@
-const getLocation = () => {
-    return fetch(`https://ipinfo.io/json?token=c57e405230649f`).then((response) => {
+const getLocation = async () => {
+    const response = await fetch(`https://ipinfo.io/json?token=c57e405230649f`)
+        
         if (response.status === 200) {
-            return response.json()
+            const data = await response.json()
+            return data
         } else {
             throw new Error('Unable to fetch data')
         }
-    }).then((data) => {
-        return data
-    })
+
 }
 
-const getCountry = (countryCode) => {
-    return fetch(`https://restcountries.eu/rest/v2/all`).then((response) => {
+const getCountry = async (countryCode) => {
+    const response = await fetch(`https://restcountries.eu/rest/v2/all`)
         if (response.status === 200) {
-            return response.json()
+            let data = await response.json()
+            const country = data.find((country) => country.alpha2Code === countryCode)
+            return country
         } else {
             throw new Error('Unable to fetch data')
         }
-    }).then((data) => {
-        const country = data.find((country) => country.alpha2Code === countryCode)
-        return country
-    })
+        
 }
