0c6f46c2676e568ecfd0876e352949c660dfb436
heuzin <matheusqs.arq@gmail.com>
2020-05-29 11:35:31 -0400

    getLocation fetch ipinfo request

--- requests.js
+++ requests.js
@@ -1,1 +1,11 @@
-
+const getLocation = () => {
+    return fetch(`https://ipinfo.io/json?token=c57e405230649f`).then((response) => {
+        if (response.status === 200) {
+            return response.json()
+        } else {
+            throw new Error('Unable to fetch data')
+        }
+    }).then((data) => {
+        return data.city
+    })
+}
