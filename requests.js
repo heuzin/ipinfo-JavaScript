0c3fdad81cedd8999a2e20dd58f6b509bce6900c
heuzin <matheusqs.arq@gmail.com>
2020-05-29 11:59:22 -0400

    getCountry fetch api restcountries.iu

--- requests.js
+++ requests.js
@@ -9,3 +9,16 @@ const getLocation = () => {
         return data
     })
 }
+
+const getCountry = (countryCode) => {
+    return fetch(`https://restcountries.eu/rest/v2/all`).then((response) => {
+        if (response.status === 200) {
+            return response.json()
+        } else {
+            throw new Error('Unable to fetch data')
+        }
+    }).then((data) => {
+        const country = data.find((country) => country.alpha2Code === countryCode)
+        return country
+    })
+}
